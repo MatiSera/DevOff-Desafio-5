85e37de3c6db18097295c9706f0b3ae98c20f703
MatiSera <serafinimatias2@gmail.com>
2020-11-10 22:53:11 -0300

    react & material ui

---
+++ poke-search/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Button from '@material-ui/core/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <Button variant="contained" color="primary">
+        Hola Mundo!
+      </Button>
+    </div>
+  );
+}
+
+export default App;
